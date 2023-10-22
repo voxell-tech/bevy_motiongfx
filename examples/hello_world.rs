d3af836c684062bb7473bd3a45feecc303c783a8
Nixon <voxell.technologies@gmail.com>
2023-10-22 16:17:56 +0800

    feat: use grid cubes for hello_world example

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -4,7 +4,7 @@ use bevy::{
         experimental::taa::{TemporalAntiAliasBundle, TemporalAntiAliasPlugin},
     },
     log::LogPlugin,
-    pbr::ScreenSpaceAmbientOcclusionBundle,
+    pbr::{NotShadowCaster, ScreenSpaceAmbientOcclusionBundle},
     prelude::*,
 };
 use bevy_motiongfx::prelude::*;
@@ -29,70 +29,81 @@ pub fn hello_world(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let mut cubes: Vec<Entity> = Vec::with_capacity(10);
-    let mut cube_translations: Vec<Translation> = Vec::with_capacity(10);
-    let mut cube_scales: Vec<Scale> = Vec::with_capacity(10);
-    let mut cube_colors: Vec<Emissive> = Vec::with_capacity(10);
-    let mut cube_actions: Vec<ActionMetaGroup> = Vec::with_capacity(10);
+    const WIDTH: usize = 10;
+    const HEIGHT: usize = 10;
+
+    const CAPACITY: usize = WIDTH * HEIGHT;
+
+    let mut cubes: Vec<Entity> = Vec::with_capacity(CAPACITY);
+    // States
+    let mut cube_translations: Vec<Translation> = Vec::with_capacity(CAPACITY);
+    let mut cube_scales: Vec<Scale> = Vec::with_capacity(CAPACITY);
+    let mut cube_colors: Vec<BaseColor> = Vec::with_capacity(CAPACITY);
+    // let mut cube_emissives: Vec<Emissive> = Vec::with_capacity(CAPACITY);
+    // Actions
+    let mut cube_actions: Vec<ActionMetaGroup> = Vec::with_capacity(CAPACITY);
 
     // Create cube objects (Entity)
-    let green_color: Color = Color::rgb(0.3, 0.5, 0.3);
+    let material: StandardMaterial = StandardMaterial {
+        base_color: style::GREEN.into(),
+        // emissive: Vec4::ZERO.into(),
+        // alpha_mode: AlphaMode::Multiply,
+        // unlit: true,
+        ..default()
+    };
 
-    for c in 0..10 {
-        let cube = commands
-            .spawn(PbrBundle {
-                transform: Transform::from_scale(Vec3::ONE * 0.2).with_translation(Vec3::new(
-                    -1.0,
-                    (c as f32) * 0.21 - 0.5,
-                    0.0,
-                )),
-                mesh: meshes.add(shape::Cube::default().into()),
-                material: materials.add(green_color.into()),
-                ..default()
-            })
-            .id();
-        cubes.push(cube);
+    for w in 0..WIDTH {
+        for h in 0..HEIGHT {
+            let transform: Transform = Transform::from_translation(Vec3::new(
+                (w as f32) - (WIDTH as f32) * 0.5,
+                (h as f32) - (HEIGHT as f32) * 0.5,
+                0.0,
+            ))
+            // .with_scale(Vec3::ONE * 0.25);
+            .with_scale(Vec3::ZERO);
+
+            let cube = commands
+                .spawn(PbrBundle {
+                    transform,
+                    mesh: meshes.add(shape::Cube::default().into()),
+                    material: materials.add(material.clone()),
+                    ..default()
+                })
+                .insert(NotShadowCaster)
+                .id();
+
+            cube_translations.push(Translation::from_transform(cube, &transform));
+            cube_scales.push(Scale::from_transform(cube, &transform));
+            cube_colors.push(BaseColor::from_material(cube, &material));
+            // cube_emissives.push(Emissive::from_material(cube, &material));
+
+            cubes.push(cube);
+        }
     }
 
     let mut act: ActionBuilder = ActionBuilder::new(&mut commands);
 
-    // Initialize translation
-    for c in 0..10 {
-        let id: Entity = cubes[c];
-        cube_translations.push(Translation::new(
-            id,
-            Vec3::new(-1.0, (c as f32) * 0.21 - 0.5, 0.0),
-        ));
-        cube_scales.push(Scale::new(id, Vec3::ONE * 0.2));
-        cube_colors.push(Emissive::new(id, green_color.into()));
-    }
-
     // Generate cube animations
-    for c in 0..10 {
-        cube_actions.push(
-            all(&[
-                act.play(cube_translations[c].translate(Vec3::X), 1.0),
-                act.play(cube_scales[c].scale_all(0.5), 1.0),
-                act.play(
-                    cube_colors[c].color_to((Color::AQUAMARINE * 5.0).into()),
-                    1.0,
-                ),
-            ])
-            .with_ease(ease::quart::ease_in_out),
-        );
-    }
-    for c in 0..10 {
-        cube_actions.push(
-            all(&[
-                act.play(cube_translations[c].translate(-Vec3::X), 1.0),
-                act.play(cube_scales[c].scale_all(2.0), 1.0),
-                act.play(cube_colors[c].color_to((green_color * 5.0).into()), 1.0),
-            ])
-            .with_ease(ease::circ::ease_in_out),
-        );
+    for w in 0..WIDTH {
+        for h in 0..HEIGHT {
+            let c = w * WIDTH + h;
+
+            cube_actions.push(
+                all(&[
+                    // act.play(cube_translations[c].translate(Vec3::X), 1.0),
+                    act.play(cube_scales[c].scale_all_to(0.9), 1.0),
+                    // act.play(cube_colors[c].color_to(style::GREEN), 1.0),
+                    // act.play(
+                    //     cube_emissives[c].color_to((Color::aquamarine * 2.0).into()),
+                    //     1.0,
+                    // ),
+                ])
+                .with_ease(ease::expo::ease_in_out),
+            );
+        }
     }
 
-    let action_grp: ActionMetaGroup = flow(0.1, &cube_actions);
+    let action_grp: ActionMetaGroup = flow(0.01, &cube_actions);
 
     sequence.play(action_grp);
 }
@@ -105,7 +116,7 @@ fn setup(mut commands: Commands) {
                 hdr: true,
                 ..default()
             },
-            transform: Transform::from_xyz(2.0, 2.0, 4.0).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(-0.5, -0.5, 15.0),
             tonemapping: bevy::core_pipeline::tonemapping::Tonemapping::AcesFitted,
             ..default()
         })
@@ -113,14 +124,14 @@ fn setup(mut commands: Commands) {
             composite_mode: BloomCompositeMode::EnergyConserving,
             ..default()
         })
-        .insert(FogSettings {
-            color: Color::rgba(0.05, 0.05, 0.05, 1.0),
-            falloff: FogFalloff::Linear {
-                start: 5.0,
-                end: 20.0,
-            },
-            ..default()
-        })
+        // .insert(FogSettings {
+        //     color: Color::rgba(0.05, 0.05, 0.05, 1.0),
+        //     falloff: FogFalloff::Linear {
+        //         start: 5.0,
+        //         end: 20.0,
+        //     },
+        //     ..default()
+        // })
         .insert(ScreenSpaceAmbientOcclusionBundle::default())
         .insert(TemporalAntiAliasBundle::default());
 
