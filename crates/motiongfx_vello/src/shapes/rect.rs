9773b5a67107d2701d5fed0bbada1e23049c3a04
Nixon <voxell.technologies@gmail.com>
2023-11-11 14:26:28 +0800

    feat: split Rect into Rect and RectMotion

--- crates/motiongfx_vello/src/shapes/rect.rs
+++ crates/motiongfx_vello/src/shapes/rect.rs
@@ -1,26 +1,45 @@
 use bevy_ecs::prelude::*;
-use bevy_math::prelude::*;
+use bevy_math::{DVec2, DVec4};
 use bevy_vello_renderer::vello::kurbo;
 use motiongfx_core::prelude::Action;
 
+// impl Into<kurbo::Point> for DVec2 {
+//     fn into(self) -> kurbo::Point {
+//         todo!()
+//     }
+// }
+
+#[derive(Component)]
 pub struct Rect {
-    target_id: Entity,
-    size: Vec2,
-    radius: Vec4,
+    half_size: DVec2,
+    radius: DVec4,
 }
 
 impl Rect {
-    pub fn new(target_id: Entity, size: Vec2, radius: Vec4) -> Self {
-        Self {
-            target_id,
-            size,
-            radius,
-        }
+    pub fn new(half_size: DVec2, radius: DVec4) -> Self {
+        Self { half_size, radius }
     }
 
-    // pub fn resize_to(&mut self, size: Vec2) -> Action<> {
-    //     self.size = size;
-    // }
+    pub fn kurbo_rect(&self) -> kurbo::RoundedRect {
+        kurbo::RoundedRect::new(
+            -self.half_size.x,
+            -self.half_size.y,
+            self.half_size.x,
+            self.half_size.y,
+            self.radius,
+        )
+    }
+}
 
-    // pub fn radius_to(&mut self, radius: Vec4) {}
+pub struct RectMotion {
+    target_id: Entity,
+    rect: Rect,
+}
+
+impl RectMotion {
+    pub fn new(target_id: Entity, rect: Rect) -> Self {
+        Self { target_id, rect }
+    }
+
+    // pub fn size_to(&mut self, half_size: DVec2) -> Action<Rect, Quat, EmptyRes> {}
 }
