e30a5ed92704724b3cd12dfb47a3326fc3674856
Nixon <43715558+nixon-voxell@users.noreply.github.com>
2024-03-06 17:26:33 +0800

    Sprite motion (#27)
    * feat: add SpriteMotion
    
    * chore: add bevy_sprite deps

--- crates/motiongfx_bevy/src/lib.rs
+++ crates/motiongfx_bevy/src/lib.rs
@@ -3,15 +3,18 @@ use bevy_asset::prelude::*;
 use bevy_math::prelude::*;
 use bevy_pbr::prelude::*;
 use bevy_render::prelude::*;
+use bevy_sprite::prelude::*;
 use bevy_transform::prelude::*;
 use motiongfx_core::{prelude::*, sequence::sequence_update_system};
 
+mod sprite;
 mod standard_material;
 mod transform;
 
 pub mod prelude {
     pub use crate::{
-        standard_material::StandardMaterialMotion, transform::TransformMotion, MotionGfxBevy,
+        sprite::SpriteMotion, standard_material::StandardMaterialMotion,
+        transform::TransformMotion, MotionGfxBevy,
     };
 }
 
@@ -25,6 +28,7 @@ impl Plugin for MotionGfxBevy {
                 sequence_update_system::<Transform, Vec3, EmptyRes>,
                 sequence_update_system::<Transform, Quat, EmptyRes>,
                 sequence_update_system::<Handle<StandardMaterial>, Color, Assets<StandardMaterial>>,
+                sequence_update_system::<Sprite, Color, EmptyRes>,
             ),),
         );
     }
