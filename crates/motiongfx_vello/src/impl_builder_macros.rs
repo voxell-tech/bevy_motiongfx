30bb4395e4c95ccfa201e1f1136ce10264e9fac0
Nixon <voxell.technologies@gmail.com>
2024-04-02 00:40:24 +0800

    feat: add brush, stroke, transform builder macros

--- crates/motiongfx_vello/src/impl_builder_macros.rs
+++ crates/motiongfx_vello/src/impl_builder_macros.rs
@@ -1,8 +1,19 @@
-macro_rules! impl_fill_brush_builder {
-    ($struct_name:ident, $fill:ident) => {
-        impl $struct_name {
-            pub fn test(&self) {
-                println!("{:#?}", self.$fill);
+macro_rules! impl_brush_builder {
+    ($fn_name:tt, $struct_name:ident, $brush:ident) => {
+        ::paste::paste! {
+            impl $struct_name {
+                pub fn [< with_ $fn_name _color >](mut self, color: ::bevy::render::color::Color) -> Self {
+                    self.$brush = ::bevy_vello_renderer::vello::peniko::Brush::Solid(
+                        ::bevy_vello_renderer::vello::peniko::Color::rgba(
+                            color.r() as f64,
+                            color.g() as f64,
+                            color.b() as f64,
+                            color.a() as f64,
+                        ),
+                    );
+
+                    self
+                }
             }
         }
     };
@@ -11,12 +22,49 @@ macro_rules! impl_fill_brush_builder {
 macro_rules! impl_stroke_builder {
     ($struct_name:ident, $stroke:ident) => {
         impl $struct_name {
-            pub fn test(&self) {
-                println!("{:#?}", self.$stroke);
+            pub fn with_stroke(
+                mut self,
+                stroke: ::bevy_vello_renderer::vello::kurbo::Stroke,
+            ) -> Self {
+                self.$stroke = stroke;
+
+                self
             }
         }
     };
 }
 
-pub(crate) use impl_fill_brush_builder;
+macro_rules! impl_optional_stroke_builder {
+    ($struct_name:ident, $stroke:ident) => {
+        impl $struct_name {
+            pub fn with_stroke(
+                mut self,
+                stroke: ::bevy_vello_renderer::vello::kurbo::Stroke,
+            ) -> Self {
+                self.$stroke = Some(stroke);
+
+                self
+            }
+        }
+    };
+}
+
+macro_rules! impl_transform_builder {
+    ($struct_name:ident, $transform:ident) => {
+        impl $struct_name {
+            pub fn with_transform(
+                mut self,
+                transform: ::bevy::transform::components::Transform,
+            ) -> Self {
+                self.$transform = transform;
+
+                self
+            }
+        }
+    };
+}
+
+pub(crate) use impl_brush_builder;
+pub(crate) use impl_optional_stroke_builder;
 pub(crate) use impl_stroke_builder;
+pub(crate) use impl_transform_builder;
